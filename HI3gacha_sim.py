2d4c8ef5f282766bd8567db3e03dc39d5f7504ab
ButteryRafa <104410637+ButteryRafa@users.noreply.github.com>
2022-05-21 02:53:11 -0400

    Changed from byte strings to strings

--- HI3gacha_sim.py
+++ HI3gacha_sim.py
@@ -6,12 +6,12 @@ def singlePull(rates, arankRate, srankRate, softten, pity):
     # Pull
     if   pity == 99:
         print("\tHit full pity")
-        result = [b"S-rank Valkyrie"]
+        result = ["S-rank Valkyrie"]
     elif softten == 9:
         # Gaurantee A rank or higher after missing one for 9 pulls
         # Assumes A and S rank rates are same as before but weighted out of arankRate + srankRate instead of out of 100
         print("\tHit the ten-pull gaurantee")
-        result = random.choices([b"S-rank Valkyrie", b"A-rank Valkyrie"], cum_weights=[srankRate, arankRate], k=1)
+        result = random.choices(["S-rank Valkyrie", "A-rank Valkyrie"], cum_weights=[srankRate, arankRate], k=1)
     else:
         # Regular pull
         result = random.choices(rates["Name"], cum_weights=rates["Rate"], k=1)
@@ -22,18 +22,18 @@ def singlePull(rates, arankRate, srankRate, softten, pity):
     return result[0], softten, pity
     
 def resolvePull(pull, bRankPool, aRankPool, sRankPool):
-    frag = b"frag" in pull
+    frag = "frag" in pull
 
-    if b"Valk" in pull:
-        if   b"S-rank" in pull:
+    if "Valk" in pull:
+        if   "S-rank" in pull:
             pull = random.choice(sRankPool)
-        elif b"A-rank" in pull:
+        elif "A-rank" in pull:
             pull = random.choice(aRankPool)
-        elif b"B-rank" in pull:
+        elif "B-rank" in pull:
             pull = random.choice(bRankPool)
 
     if frag:
-        pull["Name"] = pull["Name"] + b"frag"
+        pull["Name"] = pull["Name"] + " frag"
         print("\t\t\tFragment")
 
     return pull
@@ -54,8 +54,8 @@ result = [0]*pullcnt
 print(result)
 
 # Rates and Pool for Dorm Supply
-rates = np.genfromtxt("DormRates.csv", delimiter=',', dtype=None, names=True, encoding='bytes')
-valkPool = np.genfromtxt("DormCharacterPool.csv", delimiter=',', dtype=None, names=True, encoding='bytes')
+rates = np.genfromtxt("DormRates.csv", delimiter=',', dtype=None, names=True, encoding=None)
+valkPool = np.genfromtxt("DormCharacterPool.csv", delimiter=',', dtype=None, names=True, encoding=None)
 
 for r in rates:
     print(r["Name"], r["Rate"])
@@ -70,7 +70,7 @@ arankRate = 0
 # S rank rate
 for r in rates:
     #print(r)
-    if r["Name"] == b'S-rank Valkyrie':
+    if r["Name"] == 'S-rank Valkyrie':
         srankRate = r["Rate"]
         #print("Found Srank rate")
         break
@@ -79,7 +79,7 @@ for r in rates:
 # A rank rate
 for r in rates:
     #print(r)
-    if r["Name"] == b"A-rank Valkyrie":
+    if r["Name"] == "A-rank Valkyrie":
         arankRate = r["Rate"]
         #print("Found Arank rate")
         break
@@ -94,15 +94,15 @@ if arankRate == 0:
 
 print("Rates: A rank", arankRate, "; S rank", srankRate)
 
-bRankPool = [x for x in valkPool if x["Rank"] == b"B Rank"]
-aRankPool = [x for x in valkPool if x["Rank"] == b"A Rank"]
-sRankPool = [x for x in valkPool if x["Rank"] == b"S Rank"]
+bRankPool = [x for x in valkPool if x["Rank"] == "B Rank"]
+aRankPool = [x for x in valkPool if x["Rank"] == "A Rank"]
+sRankPool = [x for x in valkPool if x["Rank"] == "S Rank"]
 
 softten = 0
 pity = 0
 i = 0
 ind = 0
-_result = b"test"
+_result = "test"
 for i in range(pullcnt):
 #while b"Frag" not in _result:
     # Pull
@@ -114,10 +114,10 @@ for i in range(pullcnt):
     print(", resolved to", result[i])
     
     # Clear ten-pull pity when a S or A rank Valk is pulled    
-    if _result == b'A-rank Valkyrie':
+    if _result == 'A-rank Valkyrie':
         print("\tten-pull cleared")
         softten = 0
-    elif _result == b'S-rank Valkyrie':
+    elif _result == 'S-rank Valkyrie':
         print("\tpity reset")
         softten = 0
         pity = 0
